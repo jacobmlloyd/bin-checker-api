e154e89cd1c0222418f734e23b8ce1ef82d86d5e
Jacob Lloyd <jacob@lloyddeveloper.com>
2022-05-15 20:36:37 -0700

    added comments

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 import json
 
+# Initialize Flask API
 app = Flask(__name__)
 api = Api(app)
 
@@ -21,19 +22,25 @@ def get_database(db_name):
 
 class GetBin(Resource):
     def get(self, bin):
+        # get db instance and open bins collection
         db = get_database('bin_list')
         bins = db['bins']
+        # get all bins matching bin
         find = bins.find({'Bin': str(bin)})
         bins = [bin for bin in find]
+        # if bins found
         if len(bins) != 0:
             del bins[0]['_id']
             return bins[0]
         else:
+            # no bins found
             return jsonify({'error': '404', 'message': 'BIN Not Found'})
     def post(self):
+        # only accept get requests
         return jsonify({'error': '401', 'message': 'Unauthorized'})
 
 api.add_resource(GetBin, '/bin/<int:bin>')
 
 if __name__ == '__main__':
+    # public host and port 8393
     app.run(host = '0.0.0.0', port = '8393')
